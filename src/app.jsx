c5263eba5a432bd92728b07e28d4944a641be6cc
TimothyKarn <timtimkarn@gmail.com>
2020-11-09 13:10:24 -0800

    added proc

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Player from "./components/player";
 export default function App() {
-
   return <div className="zone-container">
     <Player skin='f2'/>
   </div>;
