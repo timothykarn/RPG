06864585c9c640864b8ec791cb04630e5c557423
TimothyKarn <timtimkarn@gmail.com>
2020-11-09 12:22:39 -0800

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Player from "./components/player";
+export default function App() {
+
+  return <div className="zone-container">
+    <Player skin='f2'/>
+  </div>;
+}
